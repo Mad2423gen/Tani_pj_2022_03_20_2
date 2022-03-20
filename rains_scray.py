688b7d61d01fe8464e3777c6fa5c424e02556735
Mad2423gen <soramimiplatz@gmail.com>
2022-03-20 20:53:01 +0900

    更新2022-03-20 IDとパスワードの操作追加

--- rains_scray.py
+++ rains_scray.py
@@ -3,28 +3,36 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome(ChromeDriverManager().install())
-# driver = webdriver.Chrome(chrome_driver_path)
+
 
 # RAINS アカウント情報 ==============================================
 
 rains_id = '125100372700' # ID
 rains_passwd = 'cosei0304' # Password
 # ログインページURL
-raint_top_url = "https://system.reins.jp/login/main/KG/GKG001200"
+rains_top_url = "https://system.reins.jp/login/main/KG/GKG001200"
 # =================================================================
 
 
 # ログインページオープン
-driver.get(raint_top_url)
-time.sleep(2.5)
+driver.get(rains_top_url)
+time.sleep(3)
 # ID入力
 driver.find_element(By.ID, "__BVID__13").click()
-time.sleep(1)
+time.sleep(0.5)
 driver.find_element(By.ID, "__BVID__13").send_keys(rains_id)
+time.sleep(1)
 # PASSWORD入力
-
-
+driver.find_element(By.ID, "__BVID__16").click()
+time.sleep(0.5)
+driver.find_element(By.ID, "__BVID__16").send_keys(rains_passwd)
+time.sleep(1)
+driver.find_element(By.CSS_SELECTOR,
+                         ".b-custom-control-lg > .custom-control-label").click()
 
 
 time.sleep(3)
