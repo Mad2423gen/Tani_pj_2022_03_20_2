47c9194ba442a16402132a850e73c7da8076ca7f
Mad2423gen <soramimiplatz@gmail.com>
2022-03-23 23:00:49 +0900

    更新2022-03-23 メインページの最後までの動作

--- rains_scray.py
+++ rains_scray.py
@@ -32,37 +32,51 @@ driver.find_element(By.ID, "__BVID__16").click()
 time.sleep(0.5)
 driver.find_element(By.ID, "__BVID__16").send_keys(rains_passwd)
 time.sleep(1)
+# 「ガイドラインを尊守します」チェックボックスクリック
 driver.find_element(By.CSS_SELECTOR,
                          ".b-custom-control-lg > .custom-control-label").click()
 time.sleep(0.5)
+
 driver.find_element(By.CSS_SELECTOR, ".btn").click()
-time.sleep(3)
+# ログインボタン
+time.sleep(5)
+# 売買物件検索
 driver.find_element(By.CSS_SELECTOR,
                          ".card:nth-child(2) .row:nth-child(3) > .pr-2 > .btn").click()
 time.sleep(3)
+# 「検索条件を表示」クリック
 driver.find_element(By.CSS_SELECTOR,
                          ".card:nth-child(3) .align-middle:nth-child(1)").click()
-time.sleep(3)
+time.sleep(2)
+# 「保存した検索条件の選択」クリック
 dropdown = driver.find_element(By.ID, "__BVID__102")
 time.sleep(1)
+# セレクトボックス内の保存条件をクリック
 dropdown.find_element(By.XPATH, "//option[. = '01:　三重四日市　外全']").click()
 time.sleep(1)
+
 element = driver.find_element(By.ID, "__BVID__102")
 time.sleep(1)
+
 actions = ActionChains(driver)
 time.sleep(1)
+
 actions.move_to_element(element).click_and_hold().perform()
 element = driver.find_element(By.ID, "__BVID__102")
 actions = ActionChains(driver)
 time.sleep(1)
+
 actions.move_to_element(element).release().perform()
 time.sleep(2)
+# 「読込」ボタンクリック
 driver.find_element(By.CSS_SELECTOR,
                          ".mt-3:nth-child(2) > .row:nth-child(1) > .col-sm-2 > .btn").click()
 time.sleep(2)
+# ポップアップメニュー　クリック
 driver.find_element(By.CSS_SELECTOR,
                          "#\\__BVID__601___BV_modal_footer_ > .btn").click()
 time.sleep(2)
+# 下部「検索」ボタンクリック
 driver.find_element(By.CSS_SELECTOR, ".btn-primary").click()
 
 time.sleep(10)
